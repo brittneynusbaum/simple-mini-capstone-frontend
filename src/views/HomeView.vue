7e4e3d463a974f5c1d1a6ce59b97f3169f280fef
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-05 11:56:45 -0700

    adds start to create action - hardcoded

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,19 +5,36 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
-      products: []
+      products: [],
+      newProductParams: [
+        {
+
+        }
+      ]
     };
   },
   created: function () {
-    console.log('loading products');
-    axios.get('http://localhost:3000/products').then(response => {
-      console.log(response.data);
-      this.products = response.data
-    })
+    this.indexProducts();
   },
   methods: {
     indexProducts: function () {
-
+      console.log('loading products');
+      axios.get('http://localhost:3000/products.json').then(response => {
+        console.log(response.data);
+        this.products = response.data
+      });
+    },
+    createProduct: function () {
+      console.log('creating product');
+      var newProductParams = {
+        name: "Pen",
+        description: "Good for writing",
+        price: 15,
+        image_url: "https://m.media-amazon.com/images/I/711F6-eLS6L._AC_SS450_.jpg"
+      }
+      axios.post('http://localhost:3000/products.json', newProductParams).then(response => {
+        console.log(response.data)
+      })
     }
   }
 };
@@ -26,6 +43,8 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <!-- <input v-model /> -->
+    <button v-on:click="createProduct()">Add product</button>
     <div v-for="product in products" v-bind:key="product.id">{{ product.name }}</div>
   </div>
 </template>
