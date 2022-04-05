2ddcd1724063ff9b16608bf229899c1d18c73151
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-05 10:52:49 -0700

    adds index action on frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function () {
+    console.log('loading products');
+    axios.get('http://localhost:3000/products').then(response => {
+      console.log(response.data);
+      this.products = response.data
+    })
+  },
+  methods: {
+    indexProducts: function () {
+
+    }
+  }
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="product in products" v-bind:key="product.id">{{ product.name }}</div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>
