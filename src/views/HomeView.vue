cbae9ac778e342faaf318e577502f2534b4fbcf2
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-13 22:33:52 -0700

    adds character count to body

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,12 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       products: [],
-      newProductParams: {},
+      newProductParams: {
+        name: "",
+        description: "",
+        price: "",
+        image_url: ""
+      },
       currentProduct: {},
       editProductParams: {}
     };
@@ -51,12 +56,12 @@ export default {
 
       });
     },
-    destroyProduct: function (theProduct) {
-      console.log('destroying product');
-      axios.delete(`http://localhost:3000/products`).then(response => {
-        console.log(response.data);
-      });
-    }
+    // destroyProduct: function (theProduct) {
+    //   console.log('destroying product');
+    //   axios.delete(`http://localhost:3000/products`).then(response => {
+    //     console.log(response.data);
+    //   });
+    // }
   }
 };
 </script>
@@ -72,6 +77,8 @@ export default {
       Description:
       <input v-model="newProductParams.description" />
     </p>
+    <small v-if="newProductParams.description.length > 0">{{ newProductParams.description.length }} character
+      count</small>
     <p>
       Price:
       <input v-model="newProductParams.price" />
@@ -83,37 +90,38 @@ export default {
     <button v-on:click="createProduct()">Add product</button>
     <div v-for="product in products" v-bind:key="product.id">
       {{ product.name }}
-      <button v-on:click="showProduct(product)">More Info</button>
-      <dialog id="product-details">
-        <form method="dialog">
-          <h1>Product info</h1>
-          <p>Name: {{ currentProduct.name }}</p>
-          <p>Description: {{ currentProduct.description }}</p>
-          <p>Price: {{ currentProduct.price }}</p>
-          <p>Image: {{ currentProduct.image_url }}</p>
-          <button>Close</button>
-        </form>
-        <p>
-          Name:
-          <input v-model="editProductParams.name" />
-        </p>
-        <p>
-          Description:
-          <input v-model="editProductParams.description" />
-        </p>
-        <p>
-          Price:
-          <input v-model="editProductParams.price" />
-        </p>
-        <p>
-          Image Url:
-          <input v-model="editProductParams.image_url" />
-        </p>
-        <button v-on:click="updateProduct()">Update Product</button>
-        <button v-on:click="destroyProduct(currentProduct)">Delete Product</button>
-      </dialog>
     </div>
+    <button v-on:click="showProduct(product)">More Info</button>
+    <dialog id="product-details">
+      <form method="dialog">
+        <h1>Product info</h1>
+        <p>Name: {{ currentProduct.name }}</p>
+        <p>Description: {{ currentProduct.description }}</p>
+        <p>Price: {{ currentProduct.price }}</p>
+        <p>Image: {{ currentProduct.image_url }}</p>
+        <button>Close</button>
+      </form>
+      <p>
+        Name:
+        <input v-model="editProductParams.name" />
+      </p>
+      <p>
+        Description:
+        <input v-model="editProductParams.description" />
+      </p>
+      <p>
+        Price:
+        <input v-model="editProductParams.price" />
+      </p>
+      <p>
+        Image Url:
+        <input v-model="editProductParams.image_url" />
+      </p>
+      <button v-on:click="updateProduct()">Update Product</button>
+      <button v-on:click="destroyProduct(currentProduct)">Delete Product</button>
+    </dialog>
   </div>
 </template>
 
-<style></style>
+<style>
+</style>
